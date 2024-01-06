6e82ab0b12b0d18747c871a80a53c942e54508a2
erool <erool2016@yandex.ru>
2024-01-06 13:56:57 +0300

    create all

--- 2.2-databases-2/orm_migrations/website/urls.py
+++ 2.2-databases-2/orm_migrations/website/urls.py
@@ -18,7 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+import debug_toolbar
+
+from school.views import students_list
+
 urlpatterns = [
-    path('', include('school.urls')),
-    path('admin/', admin.site.urls),
+    # path('', include('school.urls')),
+    # path('admin/', admin.site.urls),
+    path('', students_list, name='students'),
+    path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
